3c1f4241183acff6c3d7545f5b3be52cbe2d5c2a
Nupurpatwal <137989160+NupurPatwal@users.noreply.github.com>
2024-06-15 13:31:35 +0530

    lecture 1
    introduction to js. its operators datatypes etc....

---
+++ js-lectures/lecture_01/basics.js
@@ -0,0 +1,21 @@
+/*let age= 23;
+age+2;
+// console.log(age);
+let eng=80;
+let hindi=90;
+let math=100;
+let avg=(eng+hindi+math)/3;
+console.log(avg);
+
+*/
+let fullName="Nupur Patwal";
+console.log(fullName.length);
+let firstName= "Nupur";
+console.log(firstName.charAt(0));
+console.log(firstName.charAt(firstName.length-1));
+console.log(firstName[firstName.length-1]);
+
+let str=" ";
+let empStr="";
+console.log(str.length);
+console.log(empStr.length);
